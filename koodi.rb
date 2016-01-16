caeeeb2172281788e8863cafc47d2e58b16f6b94
eski <esa@esa.com>
2016-01-16 21:54:09 +0200

    moro

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,13 @@
 # kirjoita koodi tänne
+
+def kertoma(luku)
+  return 0 if luku == 0
+  tulos = 1
+  luku.times do |i|
+    tulos = tulos * luku
+    luku = luku-1
+  end
+  return tulos
+end
+
+kertoma(3)
