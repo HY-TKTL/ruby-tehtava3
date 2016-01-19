0dbd8a55fdaac42bd3fda641136aefb6fe69ff8b
SusiSusi <suvi.sipila@helsinki.fi>
2016-01-19 18:06:26 +0200

    kolmas tehty

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,16 @@
 # kirjoita koodi tänne
+def kertoma(luku)
+tulos = luku
+
+luku.times do |i|
+luku = luku - 1
+if luku > 0
+tulos = tulos * luku
+end
+end
+
+return tulos
+end
+
+r = kertoma(3)
+puts r
